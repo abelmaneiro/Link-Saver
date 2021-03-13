cc2b8eb79b6b656a010b61f846e9bdabc73557f4
abelmaneiro <32059405+abelmaneiro@users.noreply.github.com>
2021-03-13 15:31:00 +0000

    Part 4 ES6 Arrow Function and This

--- app.js
+++ app.js
@@ -1,20 +1,40 @@
 const linkCategory = document.querySelector("#linkCategory");
+const submitButton = document.querySelector("#submitButton");
 let linkCategories = [];
-
-console.log(linkCategory); 
+let links = [];
 
 linkCategory.addEventListener('keydown', function(event) {
     if (event.keyCode == 188) {
         event.preventDefault();
         linkCategories.push(linkCategory.value);
         linkCategory.value = "";
-        console.log(linkCategories);
-
         displayLinkCategories();
     }
 })
 
 function displayLinkCategories() {
-    
+    console.log("Display Categories " + linkCategories);
 }
+
+submitButton.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const title = linkTitle.value;
+    const url = linkUrl.value;
+    const categories = linkCategories;
+
+    const newLink = {
+        title: title,
+        url,  // short way of doing above, JS implies url: url
+        categories
+    }
+    links.push(newLink)
+    linkTitle.value = "";
+    linkUrl.value = "";
+    linkCategory.value = "";
+    linkCategories = [];
+    displayLinkCategories();
+
+    console.log("links " + links);
+})
 
