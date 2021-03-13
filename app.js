2cb622100bc0c5c72e8363e36cf1f094d549a767
abelmaneiro <32059405+abelmaneiro@users.noreply.github.com>
2021-03-13 13:35:15 +0000

    Part 5 Dom & ES6 variables

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("Hello from javascript - updated");
+const linkCategory = document.querySelector("#linkCategory");
+let linkCategories = [];
+
+console.log(linkCategory); 
+
+linkCategory.addEventListener('keydown', function(event) {
+    if (event.keyCode == 188) {
+        event.preventDefault();
+        linkCategories.push(linkCategory.value);
+        linkCategory.value = "";
+        console.log(linkCategories);
+
+        displayLinkCategories();
+    }
+})
+
+function displayLinkCategories() {
+    
+}
+
